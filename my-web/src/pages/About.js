41ac41767c827c312a9c1dae137e8e868af5a878
PranjalSharma13 <1SI17CS077@sit.ac.in>
2024-06-09 12:36:00 -0400

    solved video problem

--- my-web/src/pages/About.js
+++ my-web/src/pages/About.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next'; // Import the useTranslation hook
 import './About.css';
-import profileImage from '../assets/images/IMG_2609.jpg'; 
+// import profileImage from '../../public/assets/videos/frontpic.mp4';
 import TechStack from '../components/TechStack';
 import Projects from './Projects';
 import Contact from '../components/Contact';
 function About() {
-    const [t, i18n] = useTranslation("global");
+    const [t] = useTranslation("global");
 
   return (
     <>
     <div className="about-section">
       <div className="profile">
-        <img src={profileImage} alt="Profile" className="profile-image" />
+        <img src={`${process.env.PUBLIC_URL}/assets/videos/frontpic.mp4`} alt="Profile" className="profile-image" />
         <div className="profile-details">
           <h1>{t("about.title")}</h1>
           <p>{t("about.discription")}</p> 
